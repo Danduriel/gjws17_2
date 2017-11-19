ecd047b3a0a2306c6da8b389633439b8269af47e
Alex Abeler <meckzankzwerg@yahoo.de>
2017-11-19 18:08:08 +0100

    Dialog Items

--- src/com/gj2/Dialog.java
+++ src/com/gj2/Dialog.java
@@ -108,37 +108,37 @@ public class Dialog extends JFrame {
 
 		//---- label1 ----
 		label1.setText("text");
-		label1.setIcon(new ImageIcon("C:\\Users\\Xen\\git\\gjws17_2\\assets\\icons\\kacka_icon.png"));
+		label1.setIcon(new ImageIcon("assets/icons/kacka_icon.png"));
 		contentPane.add(label1, "cell 1 3");
 
 		//---- label4 ----
 		label4.setText("text");
-		label4.setIcon(new ImageIcon("C:\\Users\\Xen\\git\\gjws17_2\\assets\\icons\\geld_icon.png"));
+		label4.setIcon(new ImageIcon("assets/icons/geld_icon.png"));
 		contentPane.add(label4, "cell 4 3");
 
 		//---- label2 ----
 		label2.setText("text");
-		label2.setIcon(new ImageIcon("C:\\Users\\Xen\\git\\gjws17_2\\assets\\icons\\dynamit_icon.png"));
+		label2.setIcon(new ImageIcon("assets/icons/dynamit_icon.png"));
 		contentPane.add(label2, "cell 1 4");
 
 		//---- label5 ----
 		label5.setText("text");
-		label5.setIcon(new ImageIcon("C:\\Users\\Xen\\git\\gjws17_2\\assets\\icons\\affe_icon.png"));
+		label5.setIcon(new ImageIcon("assets/icons/affe_icon.png"));
 		contentPane.add(label5, "cell 4 4");
 
 		//---- label3 ----
 		label3.setText("text");
-		label3.setIcon(new ImageIcon("C:\\Users\\Xen\\git\\gjws17_2\\assets\\icons\\flasche_icon.png"));
+		label3.setIcon(new ImageIcon("assets/icons/flasche_icon.png"));
 		contentPane.add(label3, "cell 1 5");
 
 		//---- label6 ----
 		label6.setText("text");
-		label6.setIcon(new ImageIcon("C:\\Users\\Xen\\git\\gjws17_2\\assets\\icons\\zeal_icon.png"));
+		label6.setIcon(new ImageIcon("assets/icons/zeal_icon.png"));
 		contentPane.add(label6, "cell 4 5");
 
 		//---- label7 ----
 		label7.setText("text");
-		label7.setIcon(new ImageIcon("C:\\Users\\Xen\\git\\gjws17_2\\assets\\icons\\bagger_icon.png"));
+		label7.setIcon(new ImageIcon("assets/icons/bagger_icon.png"));
 		contentPane.add(label7, "cell 1 6");
 
 		//---- btOK ----
