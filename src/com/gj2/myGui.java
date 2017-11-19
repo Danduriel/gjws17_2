7ea07f752e7d2efd0eaa1ad55dfac3d39b75f030
Xenex <kevin@hasenbach.org>
2017-11-19 16:20:24 +0100

    icons

--- src/com/gj2/myGui.java
+++ src/com/gj2/myGui.java
@@ -34,6 +34,11 @@ public class myGui extends JFrame {
 		
 		radioButton1.setSelected(false);
 		radioButton2.setSelected(false);
+		radioButton3.setSelected(false);
+		radioButton4.setSelected(false);
+		radioButton5.setSelected(false);
+		radioButton6.setSelected(false);
+		radioButton7.setSelected(false);
 	}
 
 	private void initComponents() {
@@ -51,10 +56,14 @@ public class myGui extends JFrame {
 		radioButton3 = new JRadioButton();
 		labelBeer = new JLabel();
 		labelBeerValue = new JLabel();
+		radioButton4 = new JRadioButton();
 		labelMoney = new JLabel();
 		labelMoneyValue = new JLabel();
+		radioButton5 = new JRadioButton();
 		labelFollower = new JLabel();
 		labelFollowerValue = new JLabel();
+		radioButton6 = new JRadioButton();
+		radioButton7 = new JRadioButton();
 		labelZeal = new JLabel();
 		labelZealValue = new JLabel();
 		labelProgress = new JLabel();
@@ -119,6 +128,9 @@ public class myGui extends JFrame {
 			"[]" +
 			"[]" +
 			"[]" +
+			"[]" +
+			"[]" +
+			"[]" +
 			"[]"));
 
 		//---- labelNextAction ----
@@ -129,6 +141,7 @@ public class myGui extends JFrame {
 		//---- labelTurn ----
 		labelTurn.setText("Turn:");
 		labelTurn.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		labelTurn.setIcon(null);
 		contentPane.add(labelTurn, "cell 34 0");
 
 		//---- labelTurnValue ----
@@ -137,8 +150,9 @@ public class myGui extends JFrame {
 		contentPane.add(labelTurnValue, "cell 35 0");
 
 		//---- radioButton1 ----
-		radioButton1.setText("Schmei\u00dfe eine Party");
+		radioButton1.setText("Party schmeissen");
 		radioButton1.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		radioButton1.setToolTipText("Versuche die eifrigen Affen zu verkatern.");
 		contentPane.add(radioButton1, "cell 1 1");
 
 		//---- labelAP ----
@@ -152,13 +166,15 @@ public class myGui extends JFrame {
 		contentPane.add(labelAPValue, "cell 35 1");
 
 		//---- radioButton2 ----
-		radioButton2.setText("Sprenge die Baustelle");
+		radioButton2.setText("Sabotiere Baustelle");
 		radioButton2.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		radioButton2.setToolTipText("Sabotiere die Baustelle mit etwas Explosiven.");
 		contentPane.add(radioButton2, "cell 1 2");
 
 		//---- labelDynamite ----
 		labelDynamite.setText("Dynamite:");
 		labelDynamite.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		labelDynamite.setIcon(new ImageIcon("C:\\Users\\Xen\\git\\gjws17_2\\assets\\icons\\dynamit_icon.png"));
 		contentPane.add(labelDynamite, "cell 34 2");
 
 		//---- labelDynamiteValue ----
@@ -167,19 +183,27 @@ public class myGui extends JFrame {
 		contentPane.add(labelDynamiteValue, "cell 35 2");
 
 		//---- radioButton3 ----
-		radioButton3.setText("Sprenge die Baustelle");
+		radioButton3.setText("Bun Alert!");
 		radioButton3.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		radioButton3.setToolTipText("Du lenkst alle Affen ab, in dem du flauschige Kaninchen auf der Baustelle losl\u00e4sst");
 		contentPane.add(radioButton3, "cell 1 3");
 
 		//---- labelBeer ----
 		labelBeer.setText("Bier:");
 		labelBeer.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		labelBeer.setIcon(new ImageIcon("C:\\Users\\Xen\\git\\gjws17_2\\assets\\icons\\flasche_icon.png"));
 		contentPane.add(labelBeer, "cell 34 3");
 
 		//---- labelBeerValue ----
 		labelBeerValue.setText("0");
 		labelBeerValue.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		contentPane.add(labelBeerValue, "cell 35 3");
+
+		//---- radioButton4 ----
+		radioButton4.setText("Lebensmittelvergiftung!");
+		radioButton4.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		radioButton4.setToolTipText("Du verkaufst den hungrigen Bauarbeiter-Affen ranzige Pommes.");
+		contentPane.add(radioButton4, "cell 1 4");
 
 		//---- labelMoney ----
 		labelMoney.setText("Geld:");
@@ -191,6 +215,12 @@ public class myGui extends JFrame {
 		labelMoneyValue.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		contentPane.add(labelMoneyValue, "cell 35 4");
 
+		//---- radioButton5 ----
+		radioButton5.setText("Schnuersenkel-Chaos");
+		radioButton5.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		radioButton5.setToolTipText("Du bindest die Schnuersenkel aller Bauarbeiter-Affen aneinander.");
+		contentPane.add(radioButton5, "cell 1 5");
+
 		//---- labelFollower ----
 		labelFollower.setText("Follower:");
 		labelFollower.setFont(new Font("Tahoma", Font.PLAIN, 20));
@@ -201,25 +231,38 @@ public class myGui extends JFrame {
 		labelFollowerValue.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		contentPane.add(labelFollowerValue, "cell 35 5");
 
+		//---- radioButton6 ----
+		radioButton6.setText("Bestechung");
+		radioButton6.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		radioButton6.setToolTipText("Der Busfahrer faehrt auf dem Weg zur Tempel-Baustelle 'aus Versehen' einen Umweg.");
+		contentPane.add(radioButton6, "cell 1 6");
+
+		//---- radioButton7 ----
+		radioButton7.setText("Dixis umschubsen");
+		radioButton7.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		radioButton7.setToolTipText("Das wird ein laaaanger Tag fuer alle Bauarbeiter-Affen. Und dieser Duft erst.");
+		contentPane.add(radioButton7, "cell 1 7");
+
 		//---- labelZeal ----
 		labelZeal.setText("Zeal:");
 		labelZeal.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		contentPane.add(labelZeal, "cell 34 7");
+		labelZeal.setIcon(new ImageIcon("C:\\Users\\Xen\\git\\gjws17_2\\assets\\icons\\kacka_icon.png"));
+		contentPane.add(labelZeal, "cell 34 9");
 
 		//---- labelZealValue ----
 		labelZealValue.setText("0");
 		labelZealValue.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		contentPane.add(labelZealValue, "cell 35 7");
+		contentPane.add(labelZealValue, "cell 35 9");
 
 		//---- labelProgress ----
 		labelProgress.setText("Progress:");
 		labelProgress.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		contentPane.add(labelProgress, "cell 34 8");
+		contentPane.add(labelProgress, "cell 34 10");
 
 		//---- labelProgressValue ----
 		labelProgressValue.setText("0");
 		labelProgressValue.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		contentPane.add(labelProgressValue, "cell 35 8");
+		contentPane.add(labelProgressValue, "cell 35 10");
 
 		//---- btNextTurn ----
 		btNextTurn.setText("Next Turn");
@@ -230,7 +273,7 @@ public class myGui extends JFrame {
 				btNextTurnMouseClicked(e);
 			}
 		});
-		contentPane.add(btNextTurn, "cell 35 12 1 2");
+		contentPane.add(btNextTurn, "cell 35 14 1 2");
 		pack();
 		setLocationRelativeTo(getOwner());
 
@@ -239,6 +282,10 @@ public class myGui extends JFrame {
 		buttonGroup1.add(radioButton1);
 		buttonGroup1.add(radioButton2);
 		buttonGroup1.add(radioButton3);
+		buttonGroup1.add(radioButton4);
+		buttonGroup1.add(radioButton5);
+		buttonGroup1.add(radioButton6);
+		buttonGroup1.add(radioButton7);
 		// JFormDesigner - End of component initialization  //GEN-END:initComponents
 	}
 
@@ -256,10 +303,14 @@ public class myGui extends JFrame {
 	private JRadioButton radioButton3;
 	private JLabel labelBeer;
 	private JLabel labelBeerValue;
+	private JRadioButton radioButton4;
 	private JLabel labelMoney;
 	private JLabel labelMoneyValue;
+	private JRadioButton radioButton5;
 	private JLabel labelFollower;
 	private JLabel labelFollowerValue;
+	private JRadioButton radioButton6;
+	private JRadioButton radioButton7;
 	private JLabel labelZeal;
 	private JLabel labelZealValue;
 	private JLabel labelProgress;
