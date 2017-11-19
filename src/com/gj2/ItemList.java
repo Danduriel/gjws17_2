4eea97c467d78a5a5e61353caee8f61a62ef0f38
Christina <Christina@131.220.209.152>
2017-11-19 01:39:15 +0100

    Kacke, Kacke, Bagger

--- src/com/gj2/ItemList.java
+++ src/com/gj2/ItemList.java
@@ -11,5 +11,45 @@ public class ItemList {
 							"Wenn eine Banane nicht genug ist", //Description
 							"", //Filename
 							0,0,0,0,0,0,0)); // AP Kosten,Dynamite, Beer, Money, Follower, Zeal, Progress
+		
+		//falscher Bart
+		itemList.add(new Item("falscher Bart",	//Name
+							"Falls man mal unerkant die Baustelle betreten 'muss'.", //Description
+							"", //Filename
+							0,0,0,0,0,0,0)); // AP Kosten,Dynamite, Beer, Money, Follower, Zeal, Progress
+		
+		//Kaninchen
+		itemList.add(new Item("Kaninchen",	//Name
+							"So süße, kleine Kaninchen. Was können die schon anrichten?.", //Description
+							"", //Filename
+							0,0,0,0,0,0,0)); // AP Kosten,Dynamite, Beer, Money, Follower, Zeal, Progress
+				
+		//Schnaps
+		itemList.add(new Item("Schnaps",	//Name
+							"Wenn selbst das Banana Beer nicht mehr ausreicht.", //Description
+							"", //Filename
+							0,0,0,0,0,0,0)); // AP Kosten,Dynamite, Beer, Money, Follower, Zeal, Progress
+		
+		//Kacke
+		itemList.add(new Item("Kacke",	//Name
+							"Kacke.", //Description
+							"", //Filename
+							0,0,0,0,0,0,0)); // AP Kosten,Dynamite, Beer, Money, Follower, Zeal, Progress
+		
+		//Kacke
+		itemList.add(new Item("Bagger",	//Name
+							"Ein Baggersee ist eh viel schöner, als so ein Tempel, oder?", //Description
+							"", //Filename
+							0,0,0,0,0,0,0)); // AP Kosten,Dynamite, Beer, Money, Follower, Zeal, Progress
+				
+				
+				
+			
 	}
+	
+	
+	
+	
+	
+	
 }
