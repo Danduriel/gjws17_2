3c1c7519081d838b6ac5b3e079ad1493974c1009
Xenex <kevin@hasenbach.org>
2017-11-19 02:19:06 +0100

    jjj

--- src/com/gj2/ActionCreator.java
+++ src/com/gj2/ActionCreator.java
@@ -1,12 +1,7 @@
 package com.gj2;
 
 public class ActionCreator {
-	public ActionCreator() {
-		//MyAP myAP = new MyAP("","",0,0,0,0,0,0,0);
-	}
-	
-	//Dummy ButtonFunktion
-	// Dynamite, Beer, Money, Might, Zeal, Progress, AP Kosten
+	// AP Kosten, Dynamite, Beer, Money, Might, Zeal, Progress 
 	public static MyAction clickBeerButton(){
 		MyAction tempAP = new MyAction("Party schmeissen","Versuche die eifrigen Affen zu verkatern.",0,-5,-20,0,-10,-50,-50);
 		return tempAP;
